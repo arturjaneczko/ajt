63d877f27d470b9555da7e99a8cc7ea528b555ec
Artur Janeczko <janeczko.artur@gmail.com>
2023-12-13 01:40:43 +0100

    AJT info

---
+++ src/info.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "color.h"
+#include "version.h"
+
+std::string labels() {
+	return "\033[1;30;30m \033[0m\033[1;44;37minfo\033[0m\033[1;41;37merror\033[0m\033[1;42;37minput\033[0m\033[1;43;37moutput\033[0m\033[1;45;37mmeta\033[0m";
+}
+
+std::string info() {
+	std::string info = "";
+	info += "     _       _ _____ ";
+	info += "\n";
+	info += "    / \\     | |_   _|";
+	info += "\n";
+	info += "   / _ \\ _  | | | |  ";
+	info += "\n";
+	info += "  / ___ \\ |_| | | |  ";
+	info += "\n";
+	info += " /_/   \\_\\___/  |_|  " + cyan("ver: " + version());
+	info += "\n";
+	info += cyan(" ------------------------------");
+	info += "\n";
+	info += " " + white(black("[ @ ]")) + cyan(" janeczko.artur@gmail.com");
+	info += "\n";
+	info += " " + white(black("[ ? ]")) + labels();
+	info += "\n";
+	info += "\n";
+	return info;
+}
