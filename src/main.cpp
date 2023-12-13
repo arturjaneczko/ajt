e66dd5ff10a5002ffd2049e561cc5b39312c8164
Artur Janeczko <janeczko.artur@gmail.com>
2023-12-13 23:31:19 +0100

    AJT base flow

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <string>
+#include "command.h"
+#include "info.h"
+#include "manual.h"
 
 int main() {
+	std::cout << info() << std::endl;
+	std::cout << manual() << std::endl;
+	std::string input = "";
+	while (true) {
+		std::cin >> input;
+		if (command::isQuit(input)) {
+			exit(0);
+		}
+	}
 	return 0;
 }
