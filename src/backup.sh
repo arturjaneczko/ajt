8b55f512c13a1ad500f0fc4f249cf285b4ea7b0f
Artur Janeczko <janeczko.artur@gmail.com>
2026-09-14 21:21:40 +0200

    AJT - backup refactoring

--- src/backup.sh
+++ src/backup.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 echo "Backup started"
-DIR=data/
+#rm -rf backup/
+DATA_DIR=data/
+AJT_DIR=AJT/
 BACKUP=backup/
-if [ ! -d "$DIR" ]; then
-  echo "$DIR does exist."
-  echo "Backup interrupted"
-  exit -1
+if [ ! -d $DATA_DIR ]; then
+	echo "$DATA_DATA DIR does exist."
+	echo "Backup interrupted"
+	exit -1
+fi
+if [ ! -d $AJT_DIR ]; then
+	echo "$AJT_DATA DIR does not exist!"
+	echo "Backup interrupted"
+	exit -1
 fi
 if [ ! -d "$BACKUP" ]; then
 	mkdir ${BACKUP}
 fi
 cd "${BACKUP}"
-timestamp=$(date '+%Y-%m-%d_%H:%M:%S')
+timestamp=$(date '+%Y%m%d_%H%M%S')
 extension=".zip"
 fileName="${timestamp}${extension}"
-echo "file name is: $fileName"
-DEST="${timestamp}"
-cp -r "../${DIR}" ${DEST}
-zip -r "${fileName}" "${DEST}"
-rm -rf "${DEST}"
+DATA_DEST="${timestamp}"
+AJT_DEST="AJT_${timestamp}"
 
-DIR=AJT/
-DEST="AJT_${timestamp}"
-cp -r "../${DIR}" ${DEST}
-zip -r "AJT_${fileName}" "${DEST}"
-rm -rf "${DEST}"
+cp -r "../${DATA_DIR}" ${DATA_DEST}
+zip -r ${fileName} ${DATA_DEST}
+rm -rf ${DATA_DEST}
+echo "Root 'data' backup: $fileName"
+cp -r "../${AJT_DIR}" ${AJT_DEST}
+zip -r "AJT_${fileName}" ${AJT_DEST}
+rm -rf ${AJT_DEST}
+echo "AJT 'data' backup: AJT_${fileName}"
+
 echo "Backup finished"
 exit 0
